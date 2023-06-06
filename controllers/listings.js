ce8551f1900685b987dac7d9d7058f8633cc3dc7
mohdsaadtech <mohdsaadtech@gmail.com>
2023-06-07 00:05:24 +0530

    add image upload

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,65 +1,25 @@
-import { v4 as uuidv4 } from 'uuid';
-import multer from 'multer';
+import { v4 as uuidv4 } from "uuid";
+import multer from "multer";
 import Listing from "../models/listing.js";
 import User from "../models/user.js";
-//const multer  = require('multer')
 
-const DIR = './public/images';
+export const getListings = async (req, res) => {
+  try {
+    const listings = await Listing.find();
+    res.status(200).json(listings);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, DIR);
-    },
-    filename: (req, file, cb) => {
-        const fileName = file.originalname.toLowerCase().split(' ').join('-');
-        cb(null, uuidv4() + '-' + fileName)
-    }
-});
-
-var upload = multer({
-    storage: storage,
-    fileFilter: (req, file, cb) => {
-        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-            cb(null, true);
-        } else {
-            cb(null, false);
-            return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
-        }
-    }
-});
-
-
-
-export const getListings = async (req, res) => { 
-    try {
-        const listings = await Listing.find();
-        res.status(200).json(listings);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
-
-export const getListing = async (req, res) => { 
-    const { id } = req.params;
-    console.log(id);
-    try {
-        const listing = await Listing.findById(id);
-        res.status(200).json(listing);
-    } catch (error) {
-        console.log("callling...............")
-        res.status(404).json({ message: error.message });
-    }
-}
-
-export const addListing = async (request, response) => {
-    const { user_id, title, description, address, area_name, rent, area, type, bedroom_count, bathroom_count, image_url } = request.body;
-    console.log(request.body);
-    const newListing = new Listing({ user_id, title, description, address, area_name, rent, area, type, bedroom_count, bathroom_count, image_url });
-
-    try {
-        await newListing.save();
-        response.status(201).json(newListing);
-    } catch (error) {
-        response.status(500).json({ message: error.message });  
-    }
-}
+export const getListing = async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  try {
+    const listing = await Listing.findById(id);
+    res.status(200).json(listing);
+  } catch (error) {
+    console.log("callling...............");
+    res.status(404).json({ message: error.message });
+  }
+};
--- routes/listings.js
+++ routes/listings.js
@@ -1,10 +1,82 @@
-import express from 'express';
-import { getListings, addListing, getListing } from '../controllers/listings.js';
+import express from "express";
+import { getListings, getListing } from "../controllers/listings.js";
+import { v4 as uuidv4 } from "uuid";
+import multer from "multer";
+import Listing from "../models/listing.js";
+import User from "../models/user.js";
 
 const router = express.Router();
 
-router.get('/listings', getListings);
-router.get('/listing/:id', getListing);
-router.post('/addListing', addListing);
+const DIR = "./public/images";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, DIR);
+  },
+  filename: (req, file, cb) => {
+    const fileName = file.originalname.toLowerCase().split(" ").join("-");
+    cb(null, uuidv4() + "-" + fileName);
+  },
+});
+
+var upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg"
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
+    }
+  },
+});
+
+router.get("/listings", getListings);
+router.get("/listing/:id", getListing);
+router.post(
+  "/addListing",
+  upload.single("image_url"),
+  async (request, response) => {
+    const {
+      user_id,
+      title,
+      description,
+      address,
+      area_name,
+      rent,
+      area,
+      type,
+      bedroom_count,
+      bathroom_count,
+    } = request.body;
+    console.log("request.body", request.body);
+
+    const image_url = "http://localhost:5000/images/" + request.file.filename;
+    const newListing = new Listing({
+      user_id,
+      title,
+      description,
+      address,
+      area_name,
+      rent,
+      area,
+      type,
+      bedroom_count,
+      bathroom_count,
+      image_url,
+    });
+
+    try {
+      await newListing.save();
+      response.status(201).json(newListing);
+    } catch (error) {
+      response.status(500).json({ message: error.message });
+    }
+  }
+);
 
 export default router;
