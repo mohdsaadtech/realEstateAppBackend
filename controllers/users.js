bda0256604326f6485bbd5819186b57d93eaaf9d
mohdsaadtech <mohdsaadtech@gmail.com>
2023-06-10 05:29:48 +0530

    add auth

--- controllers/users.js
+++ controllers/users.js
@@ -1,32 +1,63 @@
-import express from 'express';
+import express from "express";
 import User from "../models/user.js";
+import jwt from "jsonwebtoken";
 
-export const getUsers = async (req, res) => { 
-    try {
-        const users = await User.find();
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.json({ message: "Invalid email or password" });
+    } else {
+      //return res.json({ user: user });
+      if (user.password === password) {
+        const payload = {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+        };
+        jwt.sign(
+          payload,
+          "9YMcy6XiRiAK7KS",
+          { expiresIn: 86400 },
+          (err, token) => {
+            return res.json({
+              message: "success",
+              token: "Bearer " + token,
+              name: user.name,
+            });
+          }
+        );
+      } else {
+        return res.json({ message: "Invalid Username or Password" });
+      }
     }
-}
+    //res.status(200).json(users);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 export const addUser = async (request, response) => {
-    const { name, email, phone_no, password, role } = request.body;
-    console.log(request.body.name);
-    const newUser = new User({ name, email, phone_no, password, role });
+  const { name, email, phone_no, password, role } = request.body;
+  const newUser = new User({ name, email, phone_no, password, role });
 
-    try {
-        await newUser.save();
-        response.status(201).json(newUser);
-    } catch (error) {
-        if(error.code === 11000) {
-            if(JSON.stringify(error.keyPattern).includes("email")) {               
-                response.status(409).json({ message: "Email already exists please use different email" });
-            } else {
-                response.status(409).json({ message: "Mobile already exists please use different mobile number" });
-            } 
-        } else {
-            response.status(500).json({ message: error.message });
-        }        
+  try {
+    await newUser.save();
+    response.status(201).json(newUser);
+  } catch (error) {
+    if (error.code === 11000) {
+      if (JSON.stringify(error.keyPattern).includes("email")) {
+        response
+          .status(409)
+          .json({ message: "Email already exists please use different email" });
+      } else {
+        response.status(409).json({
+          message: "Mobile already exists please use different mobile number",
+        });
+      }
+    } else {
+      response.status(500).json({ message: error.message });
     }
-}
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,40 @@
-import express from 'express';
-import { getUsers, addUser } from '../controllers/users.js';
+import express from "express";
+import { login, addUser } from "../controllers/users.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.get('/users', getUsers);
-router.post('/addUser', addUser);
+router.post("/login", login);
+router.post("/addUser", addUser);
+
+function verifyJWT(req, res, next) {
+  // removes 'Bearer` from token
+  const token = req.headers["x-access-token"]?.split(" ")[1];
+
+  if (token) {
+    jwt.verify(token, "9YMcy6XiRiAK7KS", (err, decoded) => {
+      if (err)
+        return res.json({
+          isLoggedIn: false,
+          message: "Failed To Authenticate",
+        });
+      req.user = {};
+      req.user.id = decoded.id;
+      req.user.email = decoded.email;
+      req.user.name = decoded.name;
+      next();
+    });
+  } else {
+    res.json({ message: "Incorrect Token Given", isLoggedIn: false });
+  }
+}
+
+router.get("/isUserAuthenticated", verifyJWT, (req, res) => {
+  return res.json({
+    isLoggedIn: true,
+    email: req.user.email,
+    name: req.user.name,
+  });
+});
 
 export default router;
